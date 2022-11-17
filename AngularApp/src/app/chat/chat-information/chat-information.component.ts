0f039e959b80cf653a19c3331504b54ec8e5c292
Radu-Valentin Cornea <radu-valentin.cornea@student.tuiasi.ro>
2022-11-17 21:58:41 +0200

    Chat Module + Components for Conversation and Information

---
+++ AngularApp/src/app/chat/chat-information/chat-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-information',
+  templateUrl: './chat-information.component.html',
+  styleUrls: ['./chat-information.component.css']
+})
+export class ChatInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
