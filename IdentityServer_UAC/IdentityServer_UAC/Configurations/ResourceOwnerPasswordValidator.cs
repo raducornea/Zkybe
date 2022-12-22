736f47eab664e6248d83362802c94bfc3096c8fb
Alex Toma <alexandru-ionut.toma@student.tuiasi.ro>
2022-12-22 14:19:18 +0200

    added identity server 4 app

---
+++ IdentityServer_UAC/IdentityServer_UAC/Configurations/ResourceOwnerPasswordValidator.cs
@@ -0,0 +1,60 @@
+﻿using IdentityModel;
+using IdentityServer_UAC.Data;
+using IdentityServer4.Models;
+using IdentityServer4.Validation;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace IdentityServer_UAC.Configurations
+{
+    public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly ZkybeDbContext _dbContext;
+
+        public ResourceOwnerPasswordValidator(ZkybeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            try
+            {
+                var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Nickname == context.UserName);
+                if (user != null)
+                {
+                    if (user.Password == context.Password)
+                    {
+                        context.Result = new GrantValidationResult(
+                            subject: user.Id.ToString(),
+                            authenticationMethod: "password",
+                            claims: GetUserClaims(user));
+
+                        return;
+                    }
+
+                    context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "Incorrect password");
+                    return;
+                }
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "User does not exist.");
+                return;
+            }
+            catch (Exception ex)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "Invalid username or password");
+            }
+        }
+
+        public static Claim[] GetUserClaims(User user)
+        {
+            return new Claim[]
+            {
+            new Claim(JwtClaimTypes.Subject, user.Id.ToString()),
+            new Claim("username", user.Nickname)
+
+            //roles todo
+            // new Claim(JwtClaimTypes.Role, user.Role)
+            };
+        }
+    }
+}
---
+++ IdentityServer_UAC/IdentityServer_UAC/Data/ZkybeDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer_UAC.Data
+{
+    public class ZkybeDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ZkybeDbContext(DbContextOptions<ZkybeDbContext> options) : base(options)
+        {
+
+        }
+
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     modelBuilder.Entity<User>()
+        //         .Property(u => u.Id).HasConversion<Int32>();
+        // 
+        //     base.OnModelCreating(modelBuilder);
+        // }
+    }
+}
