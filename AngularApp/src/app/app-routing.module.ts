d094d62336cee508dd0323acf8509a93b62f5e56
Alex Toma <alexandru-ionut.toma@student.tuiasi.ro>
2023-01-10 12:41:17 +0200

    added nav button for header and fixed landing page background image

--- AngularApp/src/app/app-routing.module.ts
+++ AngularApp/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatConversationComponent } from './chat/chat-conversation/chat-conversation.component';
 import { ChatInformationComponent } from './chat/chat-information/chat-information.component';
-import { HeaderComponent } from './header/header.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -22,10 +21,6 @@ const routes: Routes = [
     // localhost:4200/register
     { 
       path: 'register', component: RegisterComponent
-    },
-    // localhost:4200/header
-    { 
-      path: 'header', component: HeaderComponent
     },
     // localhost:4200/*
     { 
